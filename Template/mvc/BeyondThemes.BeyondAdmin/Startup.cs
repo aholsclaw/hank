daf094a6634678125bba7a4beba2fcbbdc595e51
jwallesh <jwallesh@mix.wvu.edu>
2015-02-28 19:04:47 -0500

    update

---
+++ Template/mvc/BeyondThemes.BeyondAdmin/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Data.Entity;
+
+[assembly: OwinStartup(typeof(BeyondThemes.BeyondAdmin.Startup))]
+namespace BeyondThemes.BeyondAdmin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+
+            Database.SetInitializer<BeyondThemes.BeyondAdmin.Models.ProjectContext>(new DropCreateDatabaseIfModelChanges<BeyondThemes.BeyondAdmin.Models.ProjectContext>());
+           
+        }
+    }
+   
+}
